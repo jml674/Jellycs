345335e597aae2c5167964aa3c6d3195c2dea1ea
jml674 <jean-michel.lambert7@wanadoo.fr>
2014-12-10 18:32:23 +0100

    allow access to target site (xosara.com)

---
+++ jellycs/assets/scripts.js
@@ -0,0 +1,5 @@
+jellycs.functions = {
+  function1 : function() { alert("function1 separate");},
+  function2 : function() { alert("function2 separate");},
+
+};
--- jellycs/background.js
+++ jellycs/background.js
@@ -1,32 +1,32 @@
 var LOCAL_STORAGE_BADGE_CONTENT="htmlBadgesContent";
-var xosara = xosara || {};
+var jellycs = jellycs || {};
 
-xosara.monitor = {
+jellycs.monitor = {
   BADGES_CONTENT_POLLING_MNS:1,
-  DOMAIN:"www.xosara.com",
+  DOMAIN:"www.jellycs.com",
   PROTOCOL:"http://",
   htmlBadgeContent:"",
   init: function() {
      var that = this;
-     xosara.GAMES.init();
+     jellycs.GAMES.init();
      chrome.alarms.create("refreshBadgesContent", {when: 100,periodInMinutes : this.BADGES_CONTENT_POLLING_MNS});
      chrome.alarms.onAlarm.addListener(function(alarm) {
-           if (alarm.name == "refreshBadgesContent") xosara.monitor.refresh();
+           if (alarm.name == "refreshBadgesContent") jellycs.monitor.refresh();
         });
   },
   refresh:function() {
     console.log("Refreshing cache");
-    xosara.GAMES.refresh();
+    jellycs.GAMES.refresh();
   },
 };
 
-xosara.GAMES = {
+jellycs.GAMES = {
    MAX_GAMES:"8",
    HTML:"",
-   UTM:'?utm_campaign=' + xosara.monitor.DOMAIN + '&utm_medium=chrome-addon&campaign=chrome-addon',
+   UTM:'?utm_campaign=' + jellycs.monitor.DOMAIN + '&utm_medium=chrome-addon&campaign=chrome-addon',
    htmlString : "",
    init: function() {
-      this.HTML = xosara.monitor.PROTOCOL+ xosara.monitor.DOMAIN+ "/addon/json/"+this.MAX_GAMES+"/index.html";
+      this.HTML = jellycs.monitor.PROTOCOL+ jellycs.monitor.DOMAIN+ "/addon/json/"+this.MAX_GAMES+"/index.html";
       chrome.storage.local.get(LOCAL_STORAGE_BADGE_CONTENT,this.init_next);
       chrome.extension.onConnect.addListener(function(port) {
            console.debug("Connected ....."+port.name);
@@ -34,7 +34,7 @@ xosara.GAMES = {
               if (port.name == "badgeContentPopupShowing")
               {
                  console.debug("message received "+ msg);
-                 port.postMessage(xosara.GAMES.htmlString);
+                 port.postMessage(jellycs.GAMES.htmlString);
               }
            });
           });
@@ -66,9 +66,9 @@ xosara.GAMES = {
           html_string += data["ulTemplate"]["li"][1];
         }
         html_string+=data["ulTemplate"]["ul"][1];
-        //chrome.storage.local.set({xosara.GAMES.LOCAL_STORAGE_BADGE_CONTENT: html_string},function(){});
+        //chrome.storage.local.set({jellycs.GAMES.LOCAL_STORAGE_BADGE_CONTENT: html_string},function(){});
         chrome.storage.local.set({LOCAL_STORAGE_BADGE_CONTENT : html_string},function(){});
-        xosara.GAMES.htmlString = html_string;
+        jellycs.GAMES.htmlString = html_string;
         
       }      
       console.info("Data refreshed...");
@@ -76,36 +76,35 @@ xosara.GAMES = {
 
 };
 
-xosara.monitorTabs = {
-  sitesDefs:{"function":"function1","other":[{"function":"function2","sites":[{"url":"www.xosara.com"}]}]},
+jellycs.monitorTabs = {
+  sitesDefs: [{"function":"function1","sites":[{"url":"www.xosara.com"},{"url":"http://www.enjoydressup.com/"}]},{"function":"function2","sites":[{"url":"jellycs.com"}]}],
   init: function() {
-     chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {xosara.monitorTabs.changed(tabId, changeInfo, tab);}) ;
+     chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {jellycs.monitorTabs.changed(tabId, changeInfo, tab);}) ;
   },
   changed: function(tabId,changeInfo,tab) {
     if (changeInfo.status == "complete")
     {
+
        var functionCode=this.getFunctionCode(tab.url);
-       switch (functionCode) {
-          case "function1": alert("function1");
-          break;
-          case "function2": alert("function2");
-          break;
-          case "function3": alert("function3");
-          break;  
-          default: console.error("no function to call for"+url);
-       }
+       console.log("changed: function to call: "+functionCode);
+
+       if (jellycs.functions[functionCode])
+          jellycs.functions[functionCode]();
+       else
+          console.error("no function to call for"+url);
+       
     }
     
   },
   getFunctionCode:function(url) {
      var found=false;
-     var result = this.sitesDefs["function"];
-     for (var i=0;i<this.sitesDefs["other"].length;i++)
+     var result = "";
+     for (var i=0;i<this.sitesDefs.length;i++)
      {
-        var sites = this.sitesDefs["other"][i]["sites"];
-        var function_string = this.sitesDefs["other"][i]["function"];
-        //console.log("function_string="+function_string);
-        //console.log("length="+sites.length);
+        var sites = this.sitesDefs[i]["sites"];
+        var function_string = this.sitesDefs[i]["function"];
+        console.log("function_string="+function_string);
+        console.log("length="+sites.length);
         
         for (var j=0;j<sites.length;j++)
         {
@@ -123,5 +122,5 @@ xosara.monitorTabs = {
   }
 };
 
-xosara.monitor.init();
-xosara.monitorTabs.init();
+jellycs.monitor.init();
+jellycs.monitorTabs.init();
