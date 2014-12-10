a74aa9ae9b2cb7770a1177587216241ec133279c
jml674 <jean-michel.lambert7@wanadoo.fr>
2014-12-10 08:58:08 +0100

    intro of tab load detect and function selector

--- xosara/background.js
+++ xosara/background.js
@@ -29,11 +29,11 @@ xosara.GAMES = {
       this.HTML = xosara.monitor.PROTOCOL+ xosara.monitor.DOMAIN+ "/addon/json/"+this.MAX_GAMES+"/index.html";
       chrome.storage.local.get(LOCAL_STORAGE_BADGE_CONTENT,this.init_next);
       chrome.extension.onConnect.addListener(function(port) {
-           console.log("Connected ....."+port.name);
+           console.debug("Connected ....."+port.name);
            port.onMessage.addListener(function(msg) {
               if (port.name == "badgeContentPopupShowing")
               {
-                 console.log("message recieved "+ msg);
+                 console.debug("message received "+ msg);
                  port.postMessage(xosara.GAMES.htmlString);
               }
            });
@@ -71,9 +71,57 @@ xosara.GAMES = {
         xosara.GAMES.htmlString = html_string;
         
       }      
-      console.log("Data refreshed...");
+      console.info("Data refreshed...");
     },
 
 };
 
+xosara.monitorTabs = {
+  sitesDefs:{"function":"function1","other":[{"function":"function2","sites":[{"url":"www.xosara.com"}]}]},
+  init: function() {
+     chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {xosara.monitorTabs.changed(tabId, changeInfo, tab);}) ;
+  },
+  changed: function(tabId,changeInfo,tab) {
+    if (changeInfo.status == "complete")
+    {
+       var functionCode=this.getFunctionCode(tab.url);
+       switch (functionCode) {
+          case "function1": alert("function1");
+          break;
+          case "function2": alert("function2");
+          break;
+          case "function3": alert("function3");
+          break;  
+          default: console.error("no function to call for"+url);
+       }
+    }
+    
+  },
+  getFunctionCode:function(url) {
+     var found=false;
+     var result = this.sitesDefs["function"];
+     for (var i=0;i<this.sitesDefs["other"].length;i++)
+     {
+        var sites = this.sitesDefs["other"][i]["sites"];
+        var function_string = this.sitesDefs["other"][i]["function"];
+        //console.log("function_string="+function_string);
+        //console.log("length="+sites.length);
+        
+        for (var j=0;j<sites.length;j++)
+        {
+          var reg=new RegExp(sites[j]["url"]+".*");
+          if (reg.test(url))
+          {
+            result = function_string;
+            found = true;
+            break;
+          }
+        }
+        if (found) break;
+     }
+     return result;
+  }
+};
+
 xosara.monitor.init();
+xosara.monitorTabs.init();
